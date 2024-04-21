b31e382497e5ff551f8a8d61224e1e6ad1e0ba12
Richard Crowley <r@rcrowley.org>
2024-04-20 21:25:38 -0700

    Add some logging

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 	"html"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 )
 
 func main() {
+	log.Print("main.main")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("main.main HandleFunc")
 		fmt.Fprintf(w, "Hello, %q!", html.EscapeString(r.URL.Path))
 	})
-	http.ListenAndServe(fmt.Sprintf(":%d", must2(strconv.Atoi(os.Getenv("PORT")))), nil)
+	log.Print("main.main ListenAndServe: ", http.ListenAndServe(fmt.Sprintf(":%d", must2(strconv.Atoi(os.Getenv("PORT")))), nil))
 }
 
 func must2[T any](v T, err error) T {
